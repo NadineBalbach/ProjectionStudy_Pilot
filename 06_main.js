00bda1369aec53055b6bdeb17510f2a7b3442f92
NadineBalbach <43469655+NadineBalbach@users.noreply.github.com>
2026-09-15 16:04:39 +0200

    Update 06_main.js

--- 06_main.js
+++ 06_main.js
@@ -31,8 +31,8 @@ $("document").ready(function() {
         // Here, you can specify all information for the deployment
         deploy: {
             experimentID: "1",
-            serverAppURL: "https://134.2.103.83/api/submit_experiment/1",
-            // Possible deployment methods are:
+            serverAppURL: "https://134.2.103.83/api/submit_experiment/",
+            // Possible deployment methods are: https://134.2.103.83 https://csp-ms-7d89.kep2.uni-tuebingen.de
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
             deployMethod: "directLink",

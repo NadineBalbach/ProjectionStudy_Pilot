5af064bc7839fa1f7f3a55367e0594b6744af287
NadineBalbach <43469655+NadineBalbach@users.noreply.github.com>
2026-09-15 15:50:23 +0200

    Update main
    added exp no to serverappurl

--- 06_main.js
+++ 06_main.js
@@ -31,7 +31,7 @@ $("document").ready(function() {
         // Here, you can specify all information for the deployment
         deploy: {
             experimentID: "1",
-            serverAppURL: "https://134.2.103.83/api/submit_experiment/",
+            serverAppURL: "https://134.2.103.83/api/submit_experiment/1",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"

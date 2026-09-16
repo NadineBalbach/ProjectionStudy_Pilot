c68bd5cf35a3a8e5dde46c022b34808fa62c7e70
NadineBalbach <43469655+NadineBalbach@users.noreply.github.com>
2026-09-16 14:26:56 +0200

    changed server for testing

--- 06_main.js
+++ 06_main.js
@@ -30,8 +30,8 @@ $("document").ready(function() {
             ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "1",
-            serverAppURL: "https://134.2.103.83/api/submit_experiment/",
+            experimentID: "75",
+            serverAppURL: "https://magpie-cogsciprag.fly.dev/api/submit_experiment/",
             // //75 Possible deployment methods are: https://134.2.103.83 https://csp-ms-7d89.kep2.uni-tuebingen.de
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
